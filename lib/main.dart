3b17a0a1139c84aa24273ad3f380bac392f83f2d
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-06-29 18:24:31 +0530

    Add all my files

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:weather_application/weather.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Weatherapp(),
-  ));
+  runApp(Weatherapp());
 }
 class Weatherapp extends StatelessWidget {
   const Weatherapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[800],
-        appBar: AppBar(
-          title: Text('CALCULATOR'),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey,
-          elevation: 15.0,
-
-        ),
-        body: Container(
-
-        )
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: Weather(),
     );
   }
 }
---
+++ lib/weather.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Weather extends StatelessWidget {
+  const Weather({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text(
+              'SMART WEATHER'),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.search,
+              size: 30.0,
+              color: Colors.white,
+            ),
+            onPressed: () {  },
+          ),
+          actions: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0.0, 0.0, 15, 0.0),
+              child: GestureDetector(
+                onTap: () => print("Menu Click"),
+              child: SvgPicture.asset(
+                'svg/icons8-menu.svg',
+                width: 40.0,
+                height: 40.0,
+                color: Colors.white,
+              ),
+              )
+            )
+          ],
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              Image.asset(
+                'images/weather2.jpg',
+                fit: BoxFit.cover,
+                height: double.infinity,
+                width: double.infinity,
+              ),
+              Container(
+                child: Text('Veyangoda'),
+                )
+            ],
+          ),
+        )
+
+    );
+  }
+}
