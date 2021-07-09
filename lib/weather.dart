8058ab9fdc74d058e39ed0c3cefb08e4f6f6e9fd
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-07-09 23:03:52 +0530

    Update weather.dart

--- lib/weather.dart
+++ lib/weather.dart
@@ -1,173 +1,208 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class Weather extends StatelessWidget {
   const Weather({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text(
-            'SMART WEATHER'),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.search,
-            size: 30.0,
-            color: Colors.white,
-          ),
-          onPressed: () {  },
-        ),
-        actions: [
-          Container(
-              margin: EdgeInsets.fromLTRB(0.0, 0.0, 15, 0.0),
-              child: GestureDetector(
-                onTap: () => print("Menu Click"),
-                child: SvgPicture.asset(
-                  'svg/icons8-menu.svg',
-                  width: 40.0,
-                  height: 40.0,
-                  color: Colors.white,
-                ),
-              )
-          )
-        ],
-      ),
-      body: Container(
-        child: Stack(
-            children: [
-              Image.asset(
-                'images/weather2.jpg',
-                fit: BoxFit.cover,
-                height: double.infinity,
-                width: double.infinity,
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.black38),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Text(
+                      'Veyangoda',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Date Time',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Temperature',
+                      style: TextStyle(
+                        fontSize: 85,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Row(
                       children: [
-                        SizedBox(height: 120.0),
-                        Text('Veyangoda',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40.0,
-                            color: Colors.white,
-                          ),
+                        /*SvgPicture.asset(
+                          locationList[index].iconUrl,
+                          width: 34,
+                          height: 34,
+                          color: Colors.white,
+                        ),*/
+                        SizedBox(
+                          width: 10,
                         ),
-                        SizedBox(height: 5.0,),
                         Text(
-                          '07:33 PM - Tuesday, 06 Jul 2021',
+                          "Kushan",
                           style: TextStyle(
-                            fontSize: 15.0,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 40),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white30,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Wind',
+                          style: TextStyle(
+                            fontSize: 14,
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
                         ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('24\u2103',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 50.0,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Row(
-                                  children: [
-                                    SvgPicture.asset(
-                                      'svg/icons8-menu.svg',
-                                      width: 34.0,
-                                      height: 34.0,
-                                      color: Colors.white,
-                                    ),
-                                    SizedBox(width: 10.0,),
-                                    Text(
-                                      'Night',
-                                      style: TextStyle(
-                                        fontSize: 22.0,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(vertical: 40.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.white,
-                            ),
+                        /*Text(
+                          locationList[index].wind.toString(),
+                          style:TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
-
-                        ),
-
-                        Column(
-                          children: [
-                            Text('Wind',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-
+                        ),*/
                         Text(
                           'km/h',
                           style: TextStyle(
-                            fontSize: 15.0,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
                         ),
                         Stack(
                           children: [
                             Container(
-                              height: 5.0,
-                              width: 50.0,
-                              color: Colors.white54,
+                              height: 5,
+                              width: 50,
+                              color: Colors.white38,
                             ),
                             Container(
-                              height: 5.0,
-                              width: 5.0,
+                              height: 5,
+                              /*width: locationList[index].wind/2,*/
                               color: Colors.greenAccent,
-                            )
+                            ),
                           ],
-                        )
+                        ),
                       ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Rain',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '%',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Stack(
+                          children: [
+                            Container(
+                              height: 5,
+                              width: 50,
+                              color: Colors.white38,
+                            ),
 
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Humidy',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '%',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Stack(
+                          children: [
+                            Container(
+                              height: 5,
+                              width: 50,
+                              color: Colors.white38,
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
-
-              )
-
-            ]
-
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
-
     );
   }
 }
