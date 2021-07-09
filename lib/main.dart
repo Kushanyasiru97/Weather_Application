9cdd7fd5db25f1e8cc8dd1418bff046d4f89fe93
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-07-10 00:08:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Weatherapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Weather',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity
