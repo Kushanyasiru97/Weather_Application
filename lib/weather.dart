0cb51482b37120e2a7f42e3f17c2c279cd38ed2e
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-07-18 21:48:22 +0530

    Add all my files

--- lib/weather.dart
+++ lib/weather.dart
@@ -47,11 +47,121 @@ class Weather extends StatelessWidget {
                 width: double.infinity,
               ),
               Container(
-                child: Text('Veyangoda'),
-                )
-            ],
-          ),
-        )
+                decoration: BoxDecoration(color: Colors.black38),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0,70.0,0,0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Veyangoda',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          '07:33 PM - Tuesday, 06 Jul 2021',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+            ]
+
+                    ),
+                    Column(
+                      children: [
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('24\u2103',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 50.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  SizedBox(height: 5,),
+                                  SvgPicture.asset(
+                                    'svg/icons8-menu.svg',
+                                    width: 34.0,
+                                    height: 34.0,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(width: 10.0,),
+                                  Text(
+                                    'Night',
+                                    style: TextStyle(
+                                      fontSize: 22.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 40.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                          ),
+
+                        ),
+                        Column(
+                          children: [
+                            Text('Wind',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              'km/h',
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.white,
+                              ),
+                            ),
+
+                            Stack(
+                              children: [
+                                Container(
+                                  height: 5.0,
+                                  width: 50.0,
+                                  color: Colors.white54,
+                                ),
+                                Container(
+                                  height: 5.0,
+                                  width: 5.0,
+                                  color: Colors.greenAccent,
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+        ]
+
+    ),
+    ),
 
     );
   }
